ac392a1ca32c81ca457f4ff7221f1876ba1e3780
S-Susanna <s.s.s.84@yandex.ru>
2024-06-13 12:15:29 +0300

    Update test_check_swag.py
    добавлены функции

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -6,5 +6,13 @@ def test_check_icon(browser):
     swag_labs_page.visit()
     assert swag_labs_page.equal_url()
     assert swag_labs_page.exist_icon()
+   
+def test_check_usernasme(browser):
+    swag_labs_page = SwagLabs(browser)
+    swag_labs_page.visit()
     assert swag_labs_page.username()
+
+def test_check_password(browser):
+    swag_labs_page = SwagLabs(browser)
+    swag_labs_page.visit()
     assert swag_labs_page.password()
